cc09885eb7c2eb2d4fee94ea0dca262eec5019be
Mahendra Yadav <mahendra.k12@gmail.com>
2018-05-13 14:36:28 +0000

    automatically backed up by learn

--- spec/pokemon_spec.rb
+++ spec/pokemon_spec.rb
@@ -6,6 +6,7 @@ describe "Pokemon" do
     @db.execute("DROP TABLE IF EXISTS pokemon")
     @sql_runner = SQLRunner.new(@db)
     @sql_runner.execute_schema_migration_sql
+    @sql_runner.execute_create_hp_column
   end
 
   let(:pokemon) {Pokemon.new(id: 1, name: "Pikachu", type: "electric", db: @db)}
